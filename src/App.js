3ee4448e1c1f4f05686d537764fe383861d6afc9
James Pham <jamespham93@yahoo.com>
2019-01-06 19:13:16 -0800

    refractored code to pass FCC tests with better React Architecture. Also redesigned app using SASS.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
+/**Everything is done in one file to run on Codepen*/
+
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-//Markdown compiler and previewer
-import ReactMarkdown from 'react-markdown';
+// Markdown compiler and previewer (won't pass FCC test with this markdown compiler)
+// import ReactMarkdown from 'react-markdown';
 
-//intial value. serves as an example
-let initialValue = `
-# Welcome to my Markdown Previwer
+// Markdown compiler via Marked.js
+import marked from 'marked';
+
+// intial value. serves as an example. Also Global var if user wants to switch back to this example
+const defaultText = `
+# Welcome to my Markdown Previewer
 ## This is an example of another heading,
 ### and another...
 
-Here is an example of code \`<span></span>\`
+Here is an example of code \`<span>Inline text</span>\`
 
 or...
 
 \`\`\`
-//A multi-line 
-//code
+  //A multi-line 
+  //code
 
-var add = () => a + b;
+  var add = () => a + b;
 \`\`\`
 
 How about _italic_
@@ -28,15 +32,16 @@ or _**both**_!
 
 Or just ~~cross everything out~~
 
-try [links](https://www.mmafighting.com)
+try [links](https://www.freecodecamp.org)
 
 > I love block quotes! - James
 
-but sadly no tables with this cdn,
-
-but there are thematic breaks like so...
+and there are thematic breaks like so...
 
 ----
+
+### How about some images
+![React](https://daveceddia.com/images/start-small.png)
 
 - and Indentation levels
   - there are
@@ -50,61 +55,104 @@ but there are thematic breaks like so...
 * (without numbers!)
 `
 
-const Editor = ( props ) => {
-  return (
-    <div id='editor-container'>
-      <h3 class='sub-title'>Editor</h3>
-      <textarea id='editor' value={props.value} onChange={props.onChange} >  
-      </textarea>
-    </div> 
-  );
+// // ALLOWS LINE BREAKS WITH RETURN BUTTON
+// marked.setOptions({
+//   breaks: true,
+// });
+
+// // INSERTS target="_blank" INTO HREF TAGS (required for codepen links)
+// const renderer = new marked.Renderer();
+// renderer.link = function (href, title, text) {
+//   return `<a target="_blank" href="${href}">${text}` + '</a>';
+// }
+
+// Presentational Components
+const Header = (props) => {
+  return(
+    <div id='header'>
+      <h1 id='title'>Markdown Previewer</h1>
+      <div className='btn-group' id='editor-btn'>
+        <button value='clear' onClick={props.handleClick}>Clear</button>
+        <button value='default' onClick={props.handleClick}>Default</button>    
+      </div>
+
+      <div className='btn-group' id='preview-btn'>
+        <button value='HTML' onClick={props.handleClick}>HTML</button>
+        <button value='source' onClick={props.handleClick}>Source</button>
+      </div>
+    </div>
+  )
 }
 
-class App extends Component {
+const Editor = (props) => {
+  return(
+    <textarea 
+      id='editor'
+      name='content'
+      value={props.data.content}
+      onChange={props.handleChange}
+    /> 
+  )
+}
 
-  constructor ( props ) {
-    super( props) ;
-    
+const Previewer = (props) => {
+  return(
+    <div id='preview'
+        dangerouslySetInnerHTML={{__html: !props.data.isSource ? marked(props.data.content, { renderer: renderer }): null}}>
+      {props.data.isSource ? marked(props.data.content) : null} 
+    </div>
+  )
+}
+
+
+// Smart Component. Maintains state and handles all events
+class App extends Component {
+  constructor() {
+    super()
     this.state = {
-      value: initialValue,
-      preview: initialValue,
+      content: defaultText,
+      isSource: false,
     }
 
-    this.handleChange = this.handleChange.bind(this); 
-    this.convertToMarkdown = this.convertToMarkdown.bind(this);
+    this.handleChange = this.handleChange.bind(this)
+    this.handleClick = this.handleClick.bind(this)
   }
 
-  handleChange ( event ) {
-
-  //handle sync problem when editing
-  this.setState(
-      {value: event.target.value},
-      () => {
-        let input = this.state.value.slice();
-        this.convertToMarkdown(input);
-      }
-    );
-
+  handleChange(event) {
+    const {name, value} = event.target // save specific properties of textarea to local var
+    this.setState({[name]: value})
   }
 
-  convertToMarkdown ( input ) {  
-    this.setState({
-      preview : input
-    });
+  handleClick(event) {  // for button options above editor and previewer
+    const {name, value} = event.target
+    this.setState(prevState => {
+      switch(value) {
+        case 'clear':
+          return {content: ''}
+
+        case 'default':
+          return {content: defaultText}
+
+        case 'HTML':
+          return {isSource: false}
+
+        case 'source':
+          return {isSource: true}
+      } 
+    })
 
   }
 
   render() {
-    return (
-      <div className="App">
-        <h1 id='title'>Markdown Previewer</h1>
-        <Editor value={this.state.value} onChange={this.handleChange} />
-        <div id='preview'>
-          <h3 class='sub-title'>Preview</h3>
-          <ReactMarkdown source={this.state.preview} id='preview' />
+    return(
+      <div id='main'>
+        <Header handleClick={this.handleClick} />
+        <div id='display-container'>
+          <Editor data={this.state} handleChange={this.handleChange} />
+          <Previewer data={this.state} />
         </div>
       </div>
-    );
+    )
   }
 }
 
